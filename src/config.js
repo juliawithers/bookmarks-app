3238a4c7fcf048f0c9efe04a14bf339c85e0c8d9
Julia Fehr <09jwithers@gmail.com>
2020-03-14 20:32:11 -0400

    preparing to deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$gdkTDy4wza5H1U7VqDW7MuI8a6iTwfS1/c7isLhRrn/pU9BUI/xR2',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
